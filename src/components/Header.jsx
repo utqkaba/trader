a329931512e06c935da9e542117d02e8b5448155
utqkaba <utqkaba@gmail.com>
2023-10-05 18:47:13 +0300

    adding moment js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import moment from "moment/moment";
 
 function Header() {
   return (
     <div className='text-center'>
       <p className='text-2xl text-white tracking-wide font-bold'>Current Trader</p>
-      <span>Day</span>
+      <div >
+        <p>{moment().format("L")} </p>
+        <p>{moment().format("dddd")} </p>
+      </div>
     </div>
   )
 }
