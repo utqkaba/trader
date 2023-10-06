d1f4c38b032c63770fb725234d9887794aec1bbe
utqkaba <utqkaba@gmail.com>
2023-10-06 23:44:42 +0300

    idr

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import MainCard from "./components/MainCard";
 
 function App() {
   return (
-    <div className='flex flex-col h-screen items-center justify-center bg-gray-300'>
+    <div className='flex flex-col h-screen items-center justify-center bg-gradient-to-l from-gray-600 via-gray-900 to-gray-600'>
       <MainCard />
     </div>
   );
